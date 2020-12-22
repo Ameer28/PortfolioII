70c6ef3406cf05e79461b0aae5881c2a8f5da5d2
Ameer Pestana <ameer.pestana@gmail.com>
2020-12-23 00:22:02 +0800

    Change Animation for Hero and Jumbotron
    Change Animation for Hero and Jumbotron

--- app.js
+++ app.js
@@ -13,11 +13,10 @@ const tl = gsap.timeline({defaults: {ease: "power1.out"}});
 tl.fromTo(navbar, { opacity: 0}, { opacity: 1, duration: 1});
 tl.to(navbar, {y: "0%", duration: 1, stagger: 0.25});
 
-tl.fromTo(hero, { opacity: 0}, { opacity: 1, duration: 1});
-tl.to(hero, {y: "0%", duration: 0.25, stagger: 1});
+tl.to(hero, { duration: 0.75, x: 700});
 
-tl.fromTo(jumbotron, { opacity: 0}, { opacity: 1, duration: 1}); 
-tl.to(jumbotron, {y: "0%", duration: 1, stagger: 0.25});
+tl.fromTo(jumbotron, { opacity: 0}, { opacity: 1, duration: 0.75, x: -1200}); 
+tl.to(jumbotron, {y: "0%", duration: 0.75, stagger: 0.25});
 
 tl.fromTo(headers, { opacity: 0}, { opacity: 1, duration: 1}); 
 tl.to(headers, {y: "0%", duration: 1, stagger: 0.25});
