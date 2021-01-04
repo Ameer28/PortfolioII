432c82b40ae07ccb3df997acefddcecac7c99b06
Ameer Pestana <ameer.pestana@gmail.com>
2021-01-05 00:54:10 +0800

    Added Animation to Project Section
    Added Animation to Project Section and Added Tooltips to Project with Mouse Hover

--- app.js
+++ app.js
@@ -3,18 +3,29 @@ const hero = document.querySelector(".jumboImg");
 const jumbotron = document.querySelector(".heroText");
 const headers = document.querySelector(".text-right");
 const academy = document.querySelector(".academyImg");
-const eviet = document.querySelector(".evietImg");
+const eviet = document.querySelector(".evietImgObj");
 const lips = document.querySelector(".lipsImg");
 const tech = document.querySelector(".techImg");
 const icons = document.querySelector(".profileIcons");
 
 const tl = gsap.timeline({defaults: {ease: "power1.out"}});
 
-// gsap.to(navbar, { opacity: 0}, { opacity: 1, duration: 3});
-// gsap.fromTo(navbar, { opacity: 0}, { opacity: 1});
-
 gsap.from(hero, { duration: 1, x: 50 });
 gsap.to(hero, { opacity: 5, duration: 2 });
 
 tl.fromTo(jumbotron, { opacity: 0}, { opacity: 1, duration: 0.75, x: 25}); 
 tl.to(jumbotron, {y: "0%", duration: 0.5});
+
+gsap.from(headers, { opacity: 0, delay: 2});
+gsap.to(headers, { opacity: 1});
+
+gsap.timeline({
+    scrollTrigger: {
+        trigger: headers,
+    }
+});
+
+tl.from(academy, { x: 200, opacity: 0, duration: 1.5});
+gsap.from(eviet, {y: 200, opacity: 0, duration: 1.5, delay: 1.5})
+gsap.from(lips, {y: -200, opacity: 0, duration: 1.5, delay: 1.2})
+gsap.from(tech, {x: -200, opacity: 0, duration: 1.5, delay: 1})
